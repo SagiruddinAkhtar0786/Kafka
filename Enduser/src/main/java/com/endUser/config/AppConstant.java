eebb0b8151e28898da4eefea670931bdd248f89a
SagiruddinAkhtar0786 <kp19nimcet20@gmail.com>
2026-02-28 23:06:19 +0530

    Fix Enduser folder properly

---
+++ Enduser/src/main/java/com/endUser/config/AppConstant.java
@@ -0,0 +1,7 @@
+package com.endUser.config;
+
+public class AppConstant {
+
+    public static final String Location_Update_Topic="location-update-topic";
+    public static final String Group_Id="group-1";
+}
